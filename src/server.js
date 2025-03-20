1192e012199fcd2889edc9430c67899c690187e5
SultanDev <sultanrootaws@gmail.com>
2025-03-20 11:17:17 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
